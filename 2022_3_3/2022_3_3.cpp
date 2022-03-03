ca7841a0f47598d71f3bbfe18893374e1cbd1a8e
CCLCK <84182037+CCLCK@users.noreply.github.com>
2022-03-04 00:33:51 +0800

    代码demo

--- 2022_3_3/2022_3_3.cpp
+++ 2022_3_3/2022_3_3.cpp
@@ -1,1 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//class ClassA
+//{
+//public:
+//	ClassA()
+//	{
+//		cout << "ClassA()" << endl;
+//	}
+//	~ClassA()
+//	{
+//		cout << "~ClassA()" << endl;
+//	}
+//};
+//int main()
+//{
+//	ClassA* pclassA = new ClassA[5];
+//	cout << sizeof(ClassA) << endl;
+//	delete pclassA;
+//	/*delete[] pclassA;*/
+//	return 0;
+//}
+
+
+#include<iostream>
+using namespace std;
+class A
+{
+public:
+	A()
+	{
+		cout << "A()" << endl;
+	}
+	~A()
+	{
+		cout << "~A()" << endl;
+	}
+private:
+	int b;
+	int& c = b;
+	const int d = 1;
+};
+int main()
+{
+	A* a = new A;
+	//A aa;
+	return 0;
+
+}
